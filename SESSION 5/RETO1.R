9624d2b7256868697002910eb8d46eff058517be
Victor Miguel Terron <vicmictm@hotmail.com>
2021-01-21 20:53:29 -0600

    SESION 5
    SESION 5

--- SESSION 5/RETO1.R
+++ SESSION 5/RETO1.R
@@ -1,5 +1,8 @@
-#RETO 1 SESSION 6
+#RETO 1 SESSION 5
 a<-read.csv("datoslineal.csv")
+#ATTACH sirve para llamar las columnas de un df como si fueran variables
+attach(a)
+#SIN ATTACH es necesario colocar el origen$variable
 plot(a$x,a$y,main="GRAFICO DE DISPERSIÓN")
 #MODELO DE REGRESION LINEAL
 modelo1<-lm(a$y~a$x)
@@ -15,3 +18,5 @@ plot(modelo1)
 dev.off()
 # Diga si es razonable suponer para los errores aleatoriedad,
 # normalidad y varianza constante.
+summary(modelo1)
+#
