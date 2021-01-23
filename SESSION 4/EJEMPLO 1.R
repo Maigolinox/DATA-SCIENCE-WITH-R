7fc4cae159db0406da38291683f3e15e93d3fcc7
Victor Miguel Terron <vicmictm@hotmail.com>
2021-01-23 15:20:00 -0600

    avance
    avance

---
+++ SESSION 4/EJEMPLO 1.R
@@ -0,0 +1,138 @@
+# SESION 4 EJEMPLO 1
+library(ggplot2) # Utilizaremos estos paquetes para algunas gráficas
+library(reshape2)
+#PARA ENCONTRAR LA PROBABILIDAD DE QUE SE ACIERTEN EXACTAMENTE 20
+dbinom(x = 20, size = 30, prob = 0.2)
+#PARA ENCONTRAR LA PROBABILIDAD DE QUE A LO MUCHO SE ACIERTEN 20
+pbinom(x<=20,size = 30,prob = 0.2)
+#dbinom te dice cual es la probabilidad de Pr(X=x)
+#pbinom te dice cual es la probabilidad de Pr(X<=x)
+
+#Para encontrar el valor más pequeño b tal que P(X <= b) >= 0.35, es decir, 
+# el cuantil de orden 0.35, usamos
+#CUANTILES
+qbinom(p = 0.35, size = 30, prob = 0.2) # b = 5
+
+pbinom(q = 4, size = 30, prob = 0.2) # P(X <= 4) = 0.2552 < 0.35
+pbinom(q = 5, size = 30, prob = 0.2) # P(X <= 5) = 0.4275 >= 0.35
+pbinom(q = 6, size = 30, prob = 0.2) # P(X <= 6) = 0.6070 >= 0.35
+
+#MUESTRAS ALEATORIAS
+# Para obtener una muestra aleatoria de tamaño n = 1000, 
+# de la distribución binomial con parámetros como especificamos, hacemos
+
+set.seed(4857) # Establecemos una semilla, para poder
+# reproducir la muestra en el futuro
+muestra <- rbinom(n = 1000, size = 30, prob = 0.2)
+length(muestra); muestra[1:3]
+
+# Podemos observar las frecuencias absolutas de los distintos valores obtenidos
+as.data.frame(table(muestra))
+
+#TAMBIEN PODEMOS OBSERVAR LAS FRECUENCIAS RELATIVAS
+(df1 <- as.data.frame(table(muestra)/length(muestra)))
+
+valg <- as.character(df1$muestra) # distintos valores generados por rbinom
+(valg <- as.numeric(valg)) # Convertimos a números
+#las frecuencias relativas son muy parecidas a las siguientes probabilidades
+(v1 <- round(sapply(valg, dbinom, size = 30, p = 0.2), 3))
+
+#COMBINAMOS df1 y v1 en un unico dataframe
+
+
+(df2 <- cbind(df1, v1))
+(names(df2) <- c("Exitos", "FR", "Prob"))
+
+(df2 <- melt(df2)) # función del paquete reshape2
+
+#LAS FRECUENCIAS RELATIVAS SON MUY PARECIDAS A LAS PROBABILIDADES
+ggplot(df2, aes(x = Exitos, y = value, fill = variable)) + 
+  geom_bar (stat="identity", position = "dodge") # Funciones del paquete ggplot2
+#Se considera a y como value ya qwue si aplicamos el comando names
+#al origen de los datos vemos que asi se llama la columna
+names(df2)
+
+
+#DISTRIBUCIÓN NORMAL
+
+x <- seq(-4, 4, 0.01)*6 + 175 # Algunos posibles 
+# valores que puede tomar la v.a. 
+# X (mínimo: mu-4sigma, máximo: mu+4sigma)
+y <- dnorm(x, mean = 175, sd = 6) # Valores 
+# correspondientes de la función de
+# densidad de probabilidad
+plot(x, y, type = "l", xlab = "", ylab = "")#Te grafica 
+# en forma de campana los valores de x y y 
+title(main = "Densidad de Probabilidad Normal",
+      sub = expression(paste(mu == 175, " y ", sigma == 6)))#Agrega leyendas
+#a gráfico
+abline(v = 175, lwd = 2, lty = 2) # La media es 175 grafica la línea
+
+pnorm(q = 180, mean = 175, sd = 6)
+
+par(mfrow = c(2, 2))#ESTABLECE EL LIENZO EN EL PLOT PARA GRAFICAR 
+#VARIAS EN UN MISMO ESPACIO
+plot(x, y, type = "l",
+     xlab = "",
+     ylab = "")
+#AGREGA LEYENDAS AL GRÁFICO
+title(main = "Densidad de Probabilidad Normal",
+      sub = expression(paste(mu == 175,
+                             " y ",
+                             sigma == 6)))
+polygon(c(min(x), 
+          x[x<=180],
+          180),
+        c(0, y[x<=180], 0),
+        col="blue")#GRAFICA DENTRO DE LA DISTRIBUCIÓN
+#LA PROBABILIDAD DE QUE X TOME VALORES MENORES A 180
+
+#para obtener la probabilidad de que x P(x<=165), es decir, la probabilidad de
+#que X tome un valor menor o igual a 165, ejecutamos:
+
+pnorm(q = 165, mean = 175, sd = 6)
+plot(x,y,type="l",
+     xlab = "",
+     ylab = "")
+title(main = "Densidad de probabilidad normail",
+      sub = expression(paste(mu==175,"y",sigma==6)))
+polygon(c(min(x),
+          x[x<=165],165),
+        c(0,
+          y[x<=165],0),
+        col = "black")
+
+
+#PARA OBTENER P(165 <= X <= 180), es decir, la probabilidad de que X
+#tome un valor mayor o igual a 165 y menor o igual a 180, debemos correr
+
+pnorm(q = 180, mean = 175, sd = 6) - pnorm(q = 165, mean = 175, sd = 6)
+plot(x,y,type="l",
+     xlab = "",
+     ylab = "")
+title(main = "Densidad de probabilidad normail",
+      sub = expression(paste(mu==175,"y",sigma==6)))
+polygon(c(165,
+          x[x>=165&x<=180],180),
+        c(0,
+          y[x>=165&x<=180],0),
+        col = "yellow")
+
+# Para obtener P(X >= 182), es decir, la probabilidad de que X tome un valor
+# mayor o igual a 182, una alternativa es
+plot(x,y,type="l",
+     xlab = "",
+     ylab = "")
+title(main = "Densidad de probabilidad normail",
+      sub = expression(paste(mu==175,"y",sigma==6)))
+polygon(c(182,
+          x[x>=182], max(x)),
+        c(0,
+          y[x>=182], 0),
+        col="blue")
+
+
+#Para encontrar el número b, tal que P(X <= b) = 0.75, es decir,
+#el cuantil de orden 0.75, ejecutamos
+
+(b <- qnorm(p = 0.75, mean = 175, sd = 6)) 
