14f2b48524d54d1ab420fb966c039b3adb89b15d
Victor Miguel Terron <vicmictm@hotmail.com>
2021-01-24 13:56:47 -0600

    revision hasta post3
    revision hasta post 3 queda pendiende 2 y 1

--- SESSION 3/POSTWORK.R
+++ SESSION 3/POSTWORK.R
@@ -1,43 +1,29 @@
 #POSTWORK SESION 3 VICTOR MIGUEL TERRON MACIAS
-# 
-# Ahora graficaremos probabilidades (estimadas) marginales y conjuntas para el número de goles que anotan en un partido el equipo de casa o el equipo visitante.
-# 
-# 1. Con el último data frame obtenido en el postwork de la sesión 2, elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
-#   
-#   * La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x=0,1,2,)
-# 
-# * La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y=0,1,2,)
-# 
-# * La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x=0,1,2,, y=0,1,2,)
-# 
+# Ahora graficaremos probabilidades (estimadas) marginales y
+# conjuntas para el número
+# de goles que anotan en un partido el equipo de casa o el equipo visitante.
+ 
+# 1. Con el último data frame obtenido en el postwork de la sesión 2, 
+#elabora tablas de frecuencias relativas para estimar las
+# siguientes probabilidades:
+  
+# La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x=0,1,2,)
+# La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y=0,1,2,)
+# La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x=0,1,2,, y=0,1,2,)
+
 # 2. Realiza lo siguiente:
-#   * Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo de casa
-# * Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo visitante.
-# * Un HeatMap para las probabilidades conjuntas estimadas de los números de goles que anotan el equipo de casa y el equipo visitante en un partido.
+# * Un gráfico de barras para las probabilidades marginales estimadas del
+# número de goles que anota el equipo de casa
+# * Un gráfico de barras para las probabilidades marginales estimadas del
+# número de goles que anota el equipo visitante.
+# * Un HeatMap para las probabilidades conjuntas estimadas de los números 
+#de goles que anotan el equipo de casa y el equipo visitante en un partido.
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Lo primero que haremos es cargar los paquetes que usaremos más adelante. Usamos las funciones suppressWarnings y supperssMessages para que no se impriman mensajes ni advertencias al cargar el paquete.
+#Lo primero que haremos es cargar los paquetes que usaremos más adelante.
+# Usamos las funciones suppressWarnings
+# y supperssMessages para que no se impriman mensajes ni 
+# advertencias al cargar el paquete.
 suppressWarnings(suppressMessages(library(dplyr)))
 suppressWarnings(suppressMessages(library(reshape2)))
 suppressWarnings(suppressMessages(library(ggplot2)))
@@ -51,22 +37,31 @@ d1718 <- read.csv(file = url1718) # Importación de los datos a R
 d1819 <- read.csv(file = url1819)
 d1920 <- read.csv(file = url1920)
 
-#Obtenemos una mejor idea de los datos que se encuentran en los data frames con las funciones str, head, View y summary
+#Obtenemos una mejor idea de los datos que se encuentran 
+# en los data frames con las funciones str, head, View y summary
 
 str(d1718); str(d1819); str(d1920)
 head(d1718); head(d1819); head(d1920)
 View(d1718); View(d1819); View(d1920)
 summary(d1718); summary(d1819); summary(d1920)
 
-#Ahora seleccionaremos únicamente las columnas Date, HomeTeam, AwayTeam, FTHG, FTAG y FTR en cada uno de los data frames. Primero guardaremos los data frames en una lista con nombre lista y después con ayuda de las funciones lapply y select (del paquete dplyr), seleccionaremos las columnas deseadas. Los nuevos data frames quedarán guardados en nlista.
+#Ahora seleccionaremos únicamente las columnas Date, HomeTeam, 
+# AwayTeam, FTHG, FTAG y FTR en cada uno de los data frames. 
+# Primero guardaremos los data frames en una lista con nombre lista 
+# y después con ayuda de las funciones lapply y select 
+# (del paquete dplyr), seleccionaremos las columnas deseadas. 
+# Los nuevos data frames quedarán guardados en nlista.
 
 lista <- list(d1718, d1819, d1920)
 nlista <- lapply(lista, select, Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR)
 
-#Con las funciones lapply y str observaremos la estrucura de nuestros nuevos data frames
+#Con las funciones lapply y str observaremos la estrucura
+# de nuestros nuevos data frames
 lapply(nlista, str)
 
-#Arreglamos las columnas Date para que R reconozca los elementos como fechas, esto lo hacemos con las funciones mutate (paquete dplyr) y as.Date.
+#Arreglamos las columnas Date para que R reconozca 
+# los elementos como fechas, esto lo hacemos con las funciones
+# mutate (paquete dplyr) y as.Date.
 nlista[[1]] <- mutate(nlista[[1]], Date = as.Date(Date, "%d/%m/%y"))
 nlista[[2]] <- mutate(nlista[[2]], Date = as.Date(Date, "%d/%m/%Y"))
 nlista[[3]] <- mutate(nlista[[3]], Date = as.Date(Date, "%d/%m/%Y"))
@@ -74,7 +69,8 @@ nlista[[3]] <- mutate(nlista[[3]], Date = as.Date(Date, "%d/%m/%Y"))
 #Verificamos que nuestros cambios se hayan llevado a cabo
 lapply(nlista, str)
 
-#Finalmente, con ayuda de las funciones rbind y do.call combinamos los data frames contenidos en nlista como un único data frame
+#Finalmente, con ayuda de las funciones rbind y do.call 
+# combinamos los data frames contenidos en nlista como un único data frame
 data <- do.call(rbind, nlista)
 dim(data)
 str(data)
@@ -82,12 +78,16 @@ tail(data)
 View(data)
 summary(data)
 
-#Con ayuda de la función table obtenemos las estimaciones de probabilidades solicitadas
-(pcasa <- round(table(data$FTHG)/dim(data)[1], 3)) # Probabilidades marginales estimadas para los equipos que juegan en casa
+#Con ayuda de la función table obtenemos las estimaciones de 
+# probabilidades solicitadas
+(pcasa <- round(table(data$FTHG)/dim(data)[1], 3)) # Probabilidades
+# marginales estimadas para los equipos que juegan en casa
 
-(pvisita <- round(table(data$FTAG)/dim(data)[1], 3)) # Probabilidades marginales estimadas para los equipos que juegan como visitante
+(pvisita <- round(table(data$FTAG)/dim(data)[1], 3)) # Probabilidades 
+# marginales estimadas para los equipos que juegan como visitante
 
-(pcta <- round(table(data$FTHG, data$FTAG)/dim(data)[1], 3)) # Probabilidades conjuntas estimadas para los partidos
+(pcta <- round(table(data$FTHG, data$FTAG)/dim(data)[1], 3)) # Probabilidades
+# conjuntas estimadas para los partidos
 
 pcasa <- as.data.frame(pcasa)
 str(pcasa)
--- SESSION 4/RETO 2.R
+++ SESSION 4/RETO 2.R
@@ -1,1 +1,42 @@
 #RETO 2 SESION 4
+# 1. Genere 1500 muestras de tamaño 67 de la distribución exponencial con
+# parámetro 5
+# 2. Obtenga las 1500 medias correspondientes a cada una de las muestras
+# 3. Realice el histograma de frecuencias de las 1500 medias
+# 4. Encuentre la media muestral y desviación estándar muestral de las 
+# 1500 medias 
+# 5. Compare la media muestral encontrada en el paso anterior con 
+# la media real (1/5) de la población de la cual provienen las muestras 
+# 6. Compare la desviación estándar muestral encontrada con la desviación
+# estándar real (1/5) de la población de la cual provienen las muestras 
+# pero dividida por 67 (el tamaño de las muestras)
+
+library(ggplot2)
+set.seed(123)#PARA QUE TODO EL TEAM GENERE LOS MISMOS VALORES
+#PUNTO1
+d1 <- sapply(X = rep(67, 1500), FUN = rexp, rate = 5)
+class(d1)
+#PUNTO 2
+medd1<-apply(d1,2,mean)
+#SE HACE LA CONVERSION A DATA FRAME
+dfmedd1<-as.data.frame(medd1)
+class(dfmedd1)
+#PUNTO 3 HISTOGRAMA
+graficop3<-ggplot(dfmedd1,aes(medd1))+
+  geom_histogram(colour='blue',
+                 fill='red',
+                 alpha=0.5)+
+  geom_vline(xintercept=mean(medd1),
+             linetype="dashed",
+             color="black")+
+  ggtitle("Histograma 1500 medias")+
+  labs(x="Media",y="Frecuencia")+
+  theme(plot.title = element_text(hjust = 0.5, size = 16)) 
+
+#PUNTO 4
+mean(medd1)
+sd(medd1)
+#PUNTO 5
+mean(medd1); 1/5 
+#PUNTO 6
+sd(medd1);(1/5)/sqrt(67)
