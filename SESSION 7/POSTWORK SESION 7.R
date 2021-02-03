3bfc7fbd5003e4b91d6be7ae5691a1b88d27dde8
Victor Miguel Terron <vicmictm@hotmail.com>
2021-02-03 09:28:52 -0600

    summary 7
    postwork 7

--- SESSION 7/POSTWORK SESION 7.R
+++ SESSION 7/POSTWORK SESION 7.R
@@ -1,8 +1,10 @@
 #POSTWORK SESION 7
 # install.packages("mongolite")
-#install.packages("data.table")
+# install.packages("data.table")
+library(data.table)
 library(mongolite)
-
+#ES NECESARIO ISNTALAR MONGODB SHELL
+#EJECUTAR EN SHELL mongod --port 27017
 setwd("C:/Users/Victor Miguel Terron/Documents/PHASE2/DATA-SCIENCE-2PHASE/SESSION 7/")   #dependerá de donde estén guardados tus datos
 match=data.table::fread("data.csv")
 names(match)
@@ -10,18 +12,13 @@ names(match)
 my_collection = mongo(collection = "match", db = "match_games") # create connection, database and collection
 my_collection$insert(match)  #insertando el CVS a la BDD
 
-# Número de registros
-my_collection$count()
 
-# Visualizar el fichero 
+my_collection$count()
 my_collection$find()
 
-# Basic queries: ¿Cuántos goles metió como local el Real Madrid el la fecha estipulada?
 my_collection$find('{"date":"2015-12-20", "home_team":"Real Madrid"}')
 
-#Agregar otro CVS a la DDB
 my_collection = mongo(collection = "mtcars", db = "match_games") # create connection, database and collection
 my_collection$insert(mtcars)
 
-# Cerrando la conexión
 rm(my_collection)
